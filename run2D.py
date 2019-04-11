a663e57850d92efad61756cc47eb642588a04e85
Michael J. Wilson <mjwilson@lbl.gov>
2019-04-11 10:25:56 -0700

    Tidy up and commenting.

--- run2D.py
+++ run2D.py
@@ -19,11 +19,11 @@ from    nbodykit.lab                          import  BigFileMesh
 from    sklearn.model_selection               import  train_test_split
 
 
-##  Define optimizers. 
-adam    = Adam(lr=1.e-4, amsgrad=True, clipnorm=1.)
-nadam   = Nadam(lr=1.e-4, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004, clipnorm=1.)
-
 np.random.seed(seed=314)
+
+##  Define optimizers. 
+adam    =  Adam(lr=1.e-4, amsgrad=True, clipnorm=1.)
+nadam   = Nadam(lr=1.e-4, beta_1=0.9, beta_2=0.999, epsilon=None, schedule_decay=0.004, clipnorm=1.)
 
 def prep_model_2D_A(nhot):
   ##  Loosely based on:  https://arxiv.org/pdf/1807.08732.pdf
@@ -55,7 +55,7 @@ def prep_model_2D_A(nhot):
 
   return  model
 
-def prep_model_2D_B(nhot, optimizer=adam, regressor=True, loss='mse'):
+def prep_model_2D_B(nhot=1, optimizer=adam, regress=True, loss='mse'):
   print("\n\nPreparing model.\n\n")
 
   model = Sequential()
@@ -74,7 +74,7 @@ def prep_model_2D_B(nhot, optimizer=adam, regressor=True, loss='mse'):
   model.add(Dense(32, activation='relu'))
   model.add(Dropout(0.1))
 
-  if regressor:
+  if regress:
     ##  Appropriate [0, 1] output. 
     model.add(Dense(1, activation='sigmoid'))  
     model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
@@ -89,11 +89,11 @@ def prep_model_2D_B(nhot, optimizer=adam, regressor=True, loss='mse'):
 
 def pprocess(X):
   ##  Pre-process each 2D n-body slice:
-  ##  --  Roll along random axis by random int npix
+  ##  --  Roll along random axis by random int npix.
   ##  --  Random sign (should be applied to delta).
   ##  --  To be added:  Poisson sample. 
   ##  --  To be added:  Rescale amplitude by an arbitrary growth factor. 
-  ##  
+  
   axis = np.random.randint(2)
   npix = np.random.randint(128)
 
@@ -102,17 +102,20 @@ def pprocess(X):
 
   return  sign * np.roll(X, npix, axis=axis)
 
-def get_cosmos():
+def get_cosmos(nruns=1e99):
   ##  Get list of available cosmologies.                                                                                                                                           
-  cosmos    = np.loadtxt('cosmo.txt').tolist()
-  labels    = []
+  cosmos = np.loadtxt('cosmo.txt').tolist()
+  labels = []
 
+  ##  Generate labels where each cosmology is matched to nslice slices of nseeds sims. 
   for x in cosmos:
     for i in np.arange(nseeds):
       for j in np.arange(nslice):
         labels.append(x)
 
-  labels  =  np.array(labels)
+  labels = np.array(labels)
+
+  nruns  = np.minimum(nruns, len(labels) * nseeds)
 
   return  labels
 
@@ -120,58 +123,64 @@ def get_cosmos():
 if __name__ == '__main__':
   print('\n\nWelcome to MLRSD-2D.\n\n')
 
-  train, regressor = True, True
+  train, regress = True, True
 
-  optimizer  =  adam
+  optimizer      =  adam
   
-  nseeds     =    10     ##  Number of random (seed) sims available in each cosmology. 
-  nslice     =    10     ##  Sliced 3D sim. into _nslice_ (x, z) slices.  
-  nhot       =    10     ##  Label sims by bin index in f;  nhot == # of bins. 
-  nruns      =   900     ##  Limit the number of mocks input;  None == All. 
-  nsplit     =   100     ##  Split loading, storing and learning of mocks into batches of size nsplit. 
-  ntile      =     1     ##  Number of load, train epochs through the data.      
-  epochs     =     5     ##  Number of actual (keras) epochs. 
-  valid_frac =  0.15
-  
-  ##  Set up the model.
-  model     = prep_model_2D_B(None, optimizer=optimizer, regressor=regressor)
+  nseeds         =    10     ##  Number of random (seed) sims available in each cosmology. 
+  nslice         =    10     ##  Sliced 3D sim. into _nslice_ (x, z) slices.  
+  nhot           =    10     ##  Supervised:  label sims by bin index in f;  nhot == # of bins. 
+  nruns          =   900     ##  Limit the total number of mocks input;  Set to e.g. 1e99 for all. 
+  nsplit         =   100     ##  Split loading, storing and learning of mocks into batches of size nsplit. 
+  ntile          =     1     ##  Number of load, train epochs through the data.      
+  epochs         =     5     ##  Number of actual (keras) epochs. 
+  valid_frac     =  0.15     ##  Split data X into fractions for train, validate/test.    
 
-  labels    = get_cosmos()
+  ntimes         =  np.floor(nruns / nsplit)  ##  Number of splits required.      
   
-  if nruns is None:
-    nruns   =  len(labels) * nseeds
+  model          = prep_model_2D_B(None, optimizer=optimizer, regress=regress)
 
-  ntimes    =  np.floor(nruns / nsplit)
-
-  if not regressor:
-    ##  Bin sims in f and use bin index as a supervised label.
-    fmin  = .25 ** 0.545
-    fmax  = .35 ** 0.545
-  
-    ##  Number of one-hot encodings == number of bins. 
-    bins  = np.linspace(fmin, fmax, nhot)
+  labels         = get_cosmos(nruns)
 
   ##  Load sims and train in explicit batches.
   ##  Note:  **  Tile ** ntile times to go through sims (rank ordered in cosmology).
   ##         Currently, only a subset of f values are input with each tile.         
   for split in np.tile(np.arange(ntimes), ntile): 
+    ##  Zero index for this split. 
     zero  = np.int(split) * nsplit  
   
-    ##  128 x 128 x 128 pixels, with 60K items in train and 10K in validation. 
+    ##  128 x 128 pixels, for number of mocks in split x number of slices taken from 3D sim. 
     X     = np.zeros((nsplit * nslice, 128, 128, 1))
 
     ##  Loop over mocks in split. 
     for iid in np.arange(nsplit):
       print('Loading %d' % (zero + iid))
 
+      ##  Load fastpm sim. 
       fpath           = os.environ['SCRATCH'] + '/fastpm/fpm-%d-1.0000' % (zero + iid)
       
       mesh            = BigFileMesh(fpath, dataset='1/Field', mode='real', header='Header').preview()
     
       for sslice in np.arange(nslice):
-        ##  Split 3D sim into _nslice_ 2D (x, z) slices. 
-        X[iid + nsplit * sslice, :, :, 0] = mesh[:, sslice, :]
+        ##  Split 3D sim into _nslice_ 2D (x, z) slices;  Mesh returns (1 + delta). 
+        X[iid + nsplit * sslice, :, :, 0] = mesh[:, sslice, :] - 1.0
 
+
+    y = labels[zero * nslice: (zero + nsplit) * nslice, 2]
+
+    if not regress:
+      ##  Bin sims in f and use bin index as a supervised label.                                                                                                                                                                                                                                                       
+      fmin  = .25 ** 0.545
+      fmax  = .35 ** 0.545
+
+      ##  Number of one-hot encodings == number of bins.                                                                                                                                                                                                                                                               
+      bins  = np.linspace(fmin, fmax, nhot)
+
+      y     = np.digitize(y, bins)
+
+      ##  One-hot encode target column.                                                                                                                                                                                                                                                                               \
+      y     = to_categorical(y, num_classes=nhot)
+        
     if train:
       ##  Note:  horizontal and vertical flipping of 2D slices.  
       train_gen       = ImageDataGenerator(featurewise_center=False,\
@@ -186,14 +195,6 @@ if __name__ == '__main__':
 
       ##  Fit whitening params. 
       train_gen.fit(X)
-
-      y               = labels[zero * nslice: (zero + nsplit) * nslice, 2]  
-
-      if not regressor:
-        y             = np.digitize(y, bins) 
-
-        ##  One-hot encode target column.
-        y             = to_categorical(y, num_classes=nhot)
       
       ##  Image generator for continous cretion with pre-processing;  steps_per_epoch=10 * len(X_train) / 32.
       history         = model.fit_generator(train_gen.flow(X, y, batch_size=2000, shuffle=True),\
@@ -221,17 +222,17 @@ if __name__ == '__main__':
       ##  model  = model_from_json(ojson)
       ##  model.load_weights('model.h5')
 
-      if regressor:
-         model.compile(loss='mae', optimizer=adam, metrics=['accuracy'])
+      if regress:
+         model.compile(loss='mse', optimizer=optimizer, metrics=['accuracy'])
         
       else:
-        model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+        model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
-    ##  Test the predictions;  Should be on test set rather than full set. 
+    ##  Test the predictions;  
     X_train, X_test, y_train, y_test = train_test_split(X[::-1, :, : ,:], y[::-1], test_size=valid_frac)
     
-    predictions =  model.predict(X_test)
-    score       =  model.evaluate(X_test, y_test, verbose=0)
+    predictions  =   model.predict(X_test)
+    score        =  model.evaluate(X_test, y_test, verbose=0)
 
     print('Test loss:',     score[0])
     print('Test accuracy:', score[1])
